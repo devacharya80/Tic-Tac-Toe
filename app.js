e8c0bf3f5e85093d4d706bf8883e83e5b2c456ce
devacharya80 <dhanudeva80@gmail.com>
2025-12-16 12:16:45 +0530

    Guess Number Game

--- app.js
+++ app.js
@@ -1,12 +1,54 @@
-const input = document.querySelector(".input");
-const btn = document.querySelector("#btn");
-const ul = document.querySelector("ul");
+const secretNumber = Math.floor(Math.random() * 100) + 1;
+let count = 0;
+let gameOver = false;
+console.log(secretNumber);
 
-btn.addEventListener("click",()=>{
-    const val = input.value;
-    if(val == "") return;
-    const li = document.createElement("li");
-    li.innerText = val
-    ul.appendChild(li);
-    input.value = ""
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+const reset = document.querySelector(".btn");
+const message = document.querySelector("#message");
+const attemps = document.querySelector("#attemps");
+
+button.addEventListener("click",()=>{
+
+    if(gameOver) return;
+
+    const value = Number(input.value);
+
+    if(!input.value){
+        message.textContent = `Please enter a Number`;
+        return
+    }
+
+    if(value < 1 || value > 100){
+        message.textContent = "please enter a Number in range 1-100";
+        return
+    }
+    
+    count++;
+
+    if(value === secretNumber){
+        message.textContent = `Correct!, At ${count}`;
+        gameOver = true;
+        input.disabled = true
+        button.disabled = true
+    }
+    else if(value < secretNumber){
+        message.innerText = `guess higher number`;
+    }else{
+        message.innerText = `guess lower number`;
+    }
+    attemps.innerText = `Attemps ${count}`;
+    input.value = "";
+})
+
+reset.addEventListener("click",()=>{
+    gameOver = false;
+    input.value = "";
+    count = 0;
+    input.disabled = false;
+    button.disabled = false;
+
+    message.innerText = ""
+    attemps.innerText = ""
 });
